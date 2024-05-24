fdb64b7d3e0830a1a460765a1e65f595bd92473b
aoba toshiki <aoba_toshiki@uniquevision.co.jp>
2024-05-24 14:38:43 +0900

    Add: reexport sdk

--- s3_utils/src/lib.rs
+++ s3_utils/src/lib.rs
@@ -2,6 +2,13 @@ mod client;
 mod client_with_bucket;
 mod error;
 
+pub mod sdk {
+    pub mod config {
+        pub use aws_config::*;
+    }
+    pub use aws_sdk_s3::*;
+}
+
 pub use client::*;
 pub use client_with_bucket::*;
 pub use error::*;
