00f4349a1d7491b487c9e026c3533cbb6dc8b481
aoba toshiki <aoba_toshiki@uniquevision.co.jp>
2024-06-06 18:23:22 +0900

    Add: mock mode

--- ssm_utils/src/lib.rs
+++ ssm_utils/src/lib.rs
@@ -1,14 +1,22 @@
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock, RwLockWriteGuard},
+};
+
 /// ssmのClient
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Client<C = ()> {
-    ssm: aws_sdk_ssm::Client,
+    ssm: Option<aws_sdk_ssm::Client>,
     cache: C,
 }
 
 impl Client {
     /// [`aws_sdk_ssm::Client`]から[`Client`]を作ります
     pub fn from_ssm_client(ssm: aws_sdk_ssm::Client) -> Self {
-        Self { ssm, cache: () }
+        Self {
+            ssm: Some(ssm),
+            cache: (),
+        }
     }
 
     /// 環境変数から作ります
@@ -21,86 +29,149 @@ impl Client {
     pub fn from_conf<C: Into<aws_sdk_ssm::Config>>(conf: C) -> Self {
         Self::from_ssm_client(aws_sdk_ssm::Client::from_conf(conf.into()))
     }
-}
 
-impl AsRef<aws_sdk_ssm::Client> for Client {
-    fn as_ref(&self) -> &aws_sdk_ssm::Client {
-        &self.ssm
+    /// SSMの値をキャッシュできるようにします
+    pub fn with_cache(self) -> Client<RwCache> {
+        Client {
+            ssm: self.ssm,
+            cache: <RwCache as Cache>::new_cache(),
+        }
+    }
+
+    /// Mock用のモードで起動します
+    /// このモードでは、環境変数の値から確認するようになります。
+    pub fn mock() -> Client {
+        Client {
+            ssm: None,
+            cache: (),
+        }
+    }
+
+    /// Mock用のモードで起動します
+    /// このモードでは、mapの中身を確認し、その後環境変数を確認します。
+    pub fn mock_from_map(map: HashMap<String, String>) -> CachedClient {
+        Client {
+            ssm: None,
+            cache: Arc::new(RwLock::new(map)),
+        }
     }
 }
 
-impl<C> Client<C> {
+impl<C: Cache> Client<C> {
     /// `key`にあたる値をSSMから取得します。
+    /// キャッシュが有効ならキャッシュから取得します。
     pub async fn get(&self, key: &str) -> Result<String, Error> {
-        let resp = self
-            .ssm
+        // キャッシュを見る
+        if let Some(cached) = self.cache.get(key) {
+            return Ok(cached);
+        }
+        // mockか確認
+        let Some(ssm_client) = &self.ssm else {
+            // mockならenvの値も確認する
+            return std::env::var(key).map_err(|_| Error::NotFound);
+        };
+        // ssmに問い合わせる
+        let resp = ssm_client
             .get_parameter()
             .name(key)
             .set_with_decryption(Some(true))
             .send()
             .await
-            .map_err(|e| Error::Ssm(e.into()))?;
-        resp.parameter()
-            .and_then(|it| it.value())
-            .map(|it| it.to_owned())
-            .ok_or(Error::NotFound)
+            .map_err(|e| {
+                // Not found かどうか
+                if e.as_service_error()
+                    .map(|e| e.is_parameter_not_found())
+                    .unwrap_or(false)
+                {
+                    Error::NotFound
+                } else {
+                    Error::Ssm(e.into())
+                }
+            })?;
+        match resp.parameter.and_then(|it| it.value) {
+            Some(ok) => {
+                // cacheがあればそこに入れる、ないとnoop
+                self.cache.set(key, &ok);
+                Ok(ok)
+            }
+            None => Err(Error::NotFound),
+        }
+    }
+
+    /// [`aws_sdk_ssm::Client`]を取得します。
+    /// mockだとpanicします。
+    pub fn get_inner(&self) -> &aws_sdk_ssm::Client {
+        self.ssm
+            .as_ref()
+            .expect("get_inner not supported in mock mode.")
+    }
+
+    /// mockかどうか。
+    pub fn is_mock(&self) -> bool {
+        self.ssm.is_none()
     }
 }
 
-#[cfg(feature = "cache")]
-mod cache {
-    use super::*;
-    pub type Cache = quick_cache::sync::Cache<String, String>;
+pub trait Cache: Clone {
+    fn new_cache() -> Self
+    where
+        Self: Sized;
+    fn get(&self, key: &str) -> Option<String>;
+    fn set(&self, key: &str, value: &str);
+}
 
-    impl Client {
-        /// SSMの値の取得をキャッシュできるようにします。
-        ///
-        /// 内部でキャパシティーが32のキャッシュを作成します。
-        pub fn with_cache(self) -> Client<Cache> {
-            Client {
-                ssm: self.ssm,
-                cache: Cache::new(32),
-            }
-        }
+/// キャッシュしない
+impl Cache for () {
+    fn new_cache() -> Self {
+        ()
+    }
+    #[inline]
+    fn get(&self, _key: &str) -> Option<String> {
+        None
+    }
+    #[inline]
+    fn set(&self, _key: &str, _value: &str) {}
+}
 
-        /// SSMの値の取得をキャッシュできるようにします。
-        pub fn with_cache_raw(self, cache: Cache) -> Client<Cache> {
-            Client {
-                ssm: self.ssm,
-                cache,
+pub type RwCache = Arc<RwLock<HashMap<String, String>>>;
+pub type CachedClient = Client<RwCache>;
+
+impl Cache for RwCache {
+    fn new_cache() -> Self {
+        Arc::new(RwLock::new(HashMap::new()))
+    }
+    fn get(&self, key: &str) -> Option<String> {
+        self.as_ref()
+            .read()
+            .ok()
+            .and_then(|rg| rg.get(key).cloned())
+    }
+    fn set(&self, key: &str, value: &str) {
+        match self.write() {
+            Ok(mut map) => {
+                map.insert(key.to_owned(), value.to_owned());
             }
+            Err(_) => {}
         }
     }
+}
 
-    impl Client<Cache> {
-        /// キャッシュの内容を取得します
-        pub fn get_cache_store(&self) -> &Cache {
-            &self.cache
-        }
+impl CachedClient {
+    /// キャッシュを取得する
+    pub fn get_mut_cache(&self) -> Option<RwLockWriteGuard<HashMap<String, String>>> {
+        self.cache.write().ok()
+    }
 
-        /// 値を取得をSSMから取得します。
-        /// 取得した値はキャッシュされます。
-        pub async fn get_cached(&self, key: &str) -> Result<String, Error> {
-            self.cache.get_or_insert_async(key, self.get(key)).await
-        }
-
-        /// キャッシュの値をクリアします。
-        pub fn reset_cahce(&self) {
-            self.cache.clear()
-        }
-
-        /// キャッシュの中に値を入れます
-        /// テストなどでssmから実際に取得する代わりに使えます。
-        pub fn insert_to_cache(&self, key: impl Into<String>, value: impl Into<String>) {
-            self.cache.insert(key.into(), value.into())
-        }
+    /// キャッシュを消す
+    pub fn clear_cache(&self) {
+        self.cache.write().expect("poisoned lock").clear();
     }
 }
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
+    #[error("Key not found")]
+    NotFound,
     #[error(transparent)]
     Ssm(aws_sdk_ssm::Error),
-    #[error("Not found")]
-    NotFound,
 }
